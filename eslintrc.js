a6898ea992a4d90583ac63e314c2f6f487c5862e
Stefan Probst <stefanprobst@kolabnow.com>
2020-09-09 12:33:05 +0200

    chore: base on eslint-config-next-component

--- eslintrc.js
+++ eslintrc.js
@@ -1,61 +1,10 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    project: ["./tsconfig.json"],
-    sourceType: "module",
-  },
-  env: {
-    browser: true,
-    "jest/globals": true,
-    node: true,
-    es2020: true,
-  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    "@stefanprobst/eslint-config-next-component",
     "plugin:@next/next/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/@typescript-eslint",
   ],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project: "./tsconfig.json",
-      },
-    },
-    react: {
-      version: "detect",
-    },
-  },
   rules: {
-    "no-var": ["error"],
-    "prefer-const": ["error"],
     "@next/next/no-html-link-for-pages": ["error", "src/pages"],
-    "import/order": [
-      "error",
-      { alphabetize: { order: "asc", caseInsensitive: true } },
-    ],
-    "import/no-anonymous-default-export": "error",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        components: ["Link"],
-        aspects: ["invalidHref", "preferButton"],
-      },
-    ],
-    "jsx-a11y/no-onchange": "off",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
   },
   overrides: [
     {
