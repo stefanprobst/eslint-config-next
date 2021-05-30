a4d62d7c6dba84b51a263c8582fca0f0c16959fa
Stefan Probst <stefanprobst@kolabnow.com>
2021-05-30 23:17:53 +0200

    chore: fix typo

--- eslintrc.js
+++ eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
         alphabetize: {
           order: "asc",
         },
-        "newlines-between": "alywys",
+        "newlines-between": "always",
       },
     ],
     "jsx-a11y/anchor-is-valid": [
