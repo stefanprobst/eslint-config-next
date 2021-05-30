cefff496aee27c6a6e4b65bb3f1a01e734e7065d
Stefan Probst <stefanprobst@kolabnow.com>
2021-05-30 23:08:41 +0200

    feat: adjust to eslint-config-next

--- eslintrc.js
+++ eslintrc.js
@@ -1,13 +1,100 @@
 module.exports = {
+  root: true,
   extends: [
-    "@stefanprobst/eslint-config-next-component",
-    "plugin:@next/next/recommended",
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "next",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "prettier",
   ],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  settings: {},
+  rules: {
+    "no-multiple-empty-lines": "error",
+    "no-restricted-globals": [
+      "error",
+      {
+        name: "isNaN",
+        message: "Use Number.isNaN instead.",
+      },
+    ],
+    "no-var": "error",
+    "prefer-const": "error",
+    "sort-imports": "off",
+    "import/first": "error",
+    "import/namespace": "off",
+    "import/newline-after-import": "error",
+    "import/no-anonymous-default-export": "error",
+    "import/no-duplicates": "error",
+    "import/no-named-as-default": "off",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "alywys",
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "jsx-a11y/no-autofocus": [
+      "warn",
+      {
+        ignoreNonDOM: true,
+      },
+    ],
+    "jsx-a11y/no-onchange": "off",
+    "react/prop-types": "off",
+  },
   overrides: [
     {
-      files: ["*.config.js"],
+      files: ["**/*.@(ts|tsx)"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:import/typescript",
+      ],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      settings: {
+        "import/resolver": {
+          typescript: {
+            project: "./tsconfig.json",
+            alwaysTryTypes: true,
+          },
+        },
+      },
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/array-type": [
+          "error",
+          {
+            default: "generic",
+          },
+        ],
+        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/no-unnecessary-condition": "warn",
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+          },
+        ],
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
       },
     },
   ],
